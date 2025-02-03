2300f716537c68252e42926f4eb385300dc9c4f5
Anirudh221B069 <221b069@juetguna.in>
2025-02-03 15:45:10 +0530

    LE1LE2LE3

---
+++ LE1/Problem 1/Main.java
@@ -0,0 +1,12 @@
+
+public class Main
+{
+	public static void main(String[] args) {
+		Mother m= new Mother ();
+		m.x=11;
+        	m.show();
+        	Child ch=new Child (); 
+        	ch.x=12;
+        	ch.show();
+	}
+}
