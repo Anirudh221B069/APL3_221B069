65e8d2e9ab8ffdc9e5f3a7a56c0487b261327e4d
Anirudh221B069 <221b069@juetguna.in>
2025-04-21 14:22:13 +0530

    Add files via upload

---
+++ LE5/Main.java
@@ -0,0 +1,12 @@
+
+public class Main
+{
+	public static void main(String[] args) {
+	 Beverage b = new Whisky(); b.templateMethod(30);
+	 Beverage b1 = new Beer(); b1.templateMethod(30);
+	 Beverage b2 = new Rum(); b2.templateMethod(30);
+	 Beverage b3 = new Vodka(); b3.templateMethod(30);
+	 Beverage b4 = new Wine(); b4.templateMethod(30);
+	}
+	}
+
---
+++ LE5/Wine.java
@@ -0,0 +1,8 @@
+class Wine extends Beverage{
+    void addCondiment(){
+        System.out.println("add slice of lemon");
+    }
+    void stir(){
+        System.out.println("stir gently ");
+    }
+}
